afea86f9f790541b16c9c101a4fe67dbf265530b
mim2041 <suraiyamim338@gmail.com>
2026-03-18 18:06:43 +0600

    feat: food details page done

--- src/app/[locale]/(public)/(features)/food/shop/[id]/page.tsx
+++ src/app/[locale]/(public)/(features)/food/shop/[id]/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import { shops, foods } from "../../data";
+import { MdOutlineRestaurant } from "react-icons/md";
+import { FiClock } from "react-icons/fi";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const shop = shops.find((s) => s.id === id);
   const menu = foods.filter((f) => f.shopId === id);
@@ -10,7 +16,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
   if (!shop) return <div className="p-4">Shop not found</div>;
 
   return (
-    <div className="p-4">
+    <div className="p-4 bg-gray-100 max-w-7xl mx-auto">
       <Link href="/" className="text-red-600">
         ← Back
       </Link>
@@ -32,27 +38,36 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
       </div>
 
       <div className="mt-6">
-        <h3 className="font-semibold mb-2">Menu Items ({menu.length})</h3>
+        {/* <h3 className="font-semibold mb-2">Menu Items ({menu.length})</h3> */}
         <div>
-          {menu.map((m) => (
-            <div key={m.id} className="bg-white rounded-2xl p-4 shadow-sm mb-4">
-              <div className="flex justify-between">
+          <div className="flex justify-center text-red-600 text-6xl min-h-[200px] items-center">
+            <MdOutlineRestaurant className="text-red-600" />
+          </div>
+          <div className="mt-4 flex justify-between items-start bg-white rounded-2xl p-4 md:p-6 shadow-sm">
+            <div>
+              <div className="flex items-center justify-between">
                 <div>
-                  <h4 className="font-semibold">{m.name}</h4>
-                  <div className="text-sm text-gray-500">
-                    ⭐ {m.rating} • {m.time}
-                  </div>
-                  <div className="text-sm text-gray-500 mt-2">
-                    {m.desc.slice(0, 60)}
-                    {m.desc.length > 60 ? "..." : ""}
+                  <h2 className="font-semibold text-xl">{shop.name}</h2>
+                  {/* <div className="text-sm mt-2">
+                    {shop.tag && (
+                      <span className="inline-block border border-black px-2 py-1 rounded mr-2">
+                        {shop.tag || "No Tag"}
+                      </span>
+                    )} */}
                   </div>
                 </div>
                 <div className="text-right">
-                  <div className="font-bold">৳{m.price}</div>
+                  <div className="text-2xl font-bold">৳40</div>
                 </div>
               </div>
+              <div className="mt-3 text-gray-600 bg-gray-200 text-xs md:text-base p-2 rounded-md">
+                {shop.desc}
+              </div>
+              <div className="mt-4 text-sm text-gray-600">
+                ⭐ 4.4 • <FiClock className="text-white" /> {shop.hours}
+              </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
